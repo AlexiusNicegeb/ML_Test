6b682a09334e3cbd8edfd79a2d442d2832981939
Alexius Nicegeb <DEINE_GITHUB_MAIL@xyz.com>
2025-10-15 00:07:22 +0200

    Initial commit: Docker setup

---
+++ src/main.py
@@ -0,0 +1,10 @@
+﻿import numpy as np
+import pandas as pd
+
+def main():
+    print("Hello from Dockerized project!")
+    print("NumPy:", np.__version__)
+    print("Pandas:", pd.__version__)
+
+if __name__ == "__main__":
+    main()
